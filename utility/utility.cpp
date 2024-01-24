1db608a79ff30543462522ecef6805339e5ff7b4
kuznecnubas <KuznechikTV@Yandex.kz>
2024-01-24 13:17:17 +0600

    1

--- utility/utility.cpp
+++ utility/utility.cpp
@@ -233,17 +233,208 @@ string decryptPlayfairCipher(const string& text, const string& key) {
 
     return result;
 }
+
+void printMainMenu(){
+    cout << "\033[2J\033[1;1H";
+    cout << "ШИФРОВКА/ДЕШИФРОВКА" << endl;
+    cout << "1 = Шифр Цезаря" << endl;
+    cout << "2 = Шифр Виженера" << endl;
+    cout << "3 = Атбаш" << endl;
+    cout << "4 = Шифр ограждения рельсов" << endl;
+    cout << "5 = Шифр Плейфера" << endl;
+    cout << "\n0 = Выход из программы" << endl;
+}
+
+void printCipherMenu(string cipherName){
+    cout << "\033[2J\033[1;1H";
+    cout << cipherName << endl;
+    cout << "1 = Шифровка" << endl;
+    cout << "2 = Дешифровка" << endl;
+    cout << "\n0 = Выход" << endl;
+}
+
 int main(){
     system("chcp 65001");
-    while (true){
-        string input;
-        cout << "\033[2J\033[1;1H";
-        cout << "ШИФРОВКА/ДЕШИФРОВКА" << endl;
-        getline(cin, input);
 
+    string input, word, key;
+    int shift, rails;
+
+    while (true){
         if (input == "0"){
-            cout << "\nВыход";
+            cout << "\033[2J\033[1;1H";
+            cout << "Выход";
             return 0;
+        }
+        else if (input == "1"){
+            while (true) {
+                printCipherMenu("ШИФР ЦЕЗАРЯ");
+                cout << "Выберите вариант: ";
+                getline(cin, input);
+
+                if (input == "0"){
+                    input = " ";
+                    break;
+                }
+                else if (input == "1"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ШИФРОВКА" << endl;
+                    cout << "Введите сдвиг: ";
+                    cin >> shift;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Шифрованное слово: " << encryptCaesarCipher(word, shift) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+                else if (input == "2"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ДЕШИФРОВКА" << endl;
+                    cout << "Введите сдвиг: ";
+                    cin >> shift;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Дешифрованное слово: " << decryptCaesarCipher(word, shift) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+            }
+        }
+        else if (input == "2"){
+            while (true) {
+                printCipherMenu("ШИФР ВИЖЕНЕРА");
+                cout << "Выберите вариант: ";
+                getline(cin, input);
+
+                if (input == "0"){
+                    input = " ";
+                    break;
+                }
+                else if (input == "1"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ШИФРОВКА" << endl;
+                    cout << "Введите ключ: ";
+                    cin >> key;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Шифрованное слово: " << encryptVigenereCipher(word, key) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+                else if (input == "2"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ДЕШИФРОВКА" << endl;
+                    cout << "Введите ключ: ";
+                    cin >> key;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Дешифрованное слово: " << decryptVigenereCipher(word, key) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+            }
+        }
+        else if (input == "3"){
+            while (true) {
+                printCipherMenu("АТБАШ");
+                cout << "Выберите вариант: ";
+                getline(cin, input);
+
+                if (input == "0"){
+                    input = " ";
+                    break;
+                }
+                else if (input == "1"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ШИФРОВКА" << endl;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Шифрованное слово: " << encryptAtbashCipher(word) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+                else if (input == "2"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ДЕШИФРОВКА" << endl;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Дешифрованное слово: " << decryptAtbashCipher(word) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+            }
+        }
+        else if (input == "4"){
+            while (true) {
+                printCipherMenu("ШИФР ОГРАЖДЕНИЯ РЕЛЬСОВ");
+                cout << "Выберите вариант: ";
+                getline(cin, input);
+
+                if (input == "0"){
+                    input = " ";
+                    break;
+                }
+                else if (input == "1"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ШИФРОВКА" << endl;
+                    cout << "Введите кол-во рельсов: ";
+                    cin >> rails;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Шифрованное слово: " << encryptRailFenceCipher(word, rails) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+                else if (input == "2"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ДЕШИФРОВКА" << endl;
+                    cout << "Введите кол-во рельсов: ";
+                    cin >> rails;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Дешифрованное слово: " << decryptRailFenceCipher(word, rails) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+            }
+        }
+        else if (input == "5"){
+            while (true) {
+                printCipherMenu("ШИФР ВИЖЕНЕРА");
+                cout << "Выберите вариант: ";
+                getline(cin, input);
+
+                if (input == "0"){
+                    input = " ";
+                    break;
+                }
+                else if (input == "1"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ШИФРОВКА" << endl;
+                    cout << "Введите ключ: ";
+                    cin >> key;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Шифрованное слово: " << encryptVigenereCipher(word, key) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+                else if (input == "2"){
+                    cout << "\033[2J\033[1;1H";
+                    cout << "ДЕШИФРОВКА" << endl;
+                    cout << "Введите ключ: ";
+                    cin >> key;
+                    cout << "Введите слово: ";
+                    cin >> word;
+                    cout << "Дешифрованное слово: " << decryptVigenereCipher(word, key) << endl;
+                    cout << "Введите любую букву: ";
+                    cin >> word;
+                }
+            }
+        }
+        else{
+            printMainMenu();
+            cout << "Выберите шифр: ";
+            getline(cin, input);
         }
     }
 }
