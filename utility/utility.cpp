69fedb2b7051f06de242d71191c97440d257a328
kuznecnubas <KuznechikTV@Yandex.kz>
2024-01-24 10:43:29 +0600

    1

--- utility/utility.cpp
+++ utility/utility.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <stdio.h>
+#include <string>
 using namespace std;
 
 int main(){
+    system("chcp 65001");
+    while (true){
+        string input;
+        cout << "\033[2J\033[1;1H";
+        cout << "ШИФРОВКА/ДЕШИФРОВКА" << endl;
+        getline(cin, input);
 
+        if (input == "0"){
+            cout << "\nВыход";
+            return 0;
+        }
+    }
 }
