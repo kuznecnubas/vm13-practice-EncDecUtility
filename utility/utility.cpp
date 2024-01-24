456e71e1156ef11d8c916530f48ad05531ba7cfc
Максим <garkusha.06@bk.ru>
2024-01-24 10:58:22 +0600

    обнова

--- utility/utility.cpp
+++ utility/utility.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stdio.h>
 #include <string>
 using namespace std;
 string encryptCaesarCipher(const string& text, int shift) {
@@ -11,6 +10,59 @@ string encryptCaesarCipher(const string& text, int shift) {
             result += static_cast<char>((c - base + shift) % 26 + base);
         } else {
             result += c;
+        }
+    }
+
+    return result;
+}
+string decryptCaesarCipher(const string& text, int shift) {
+    string result = "";
+
+    for (char c : text) {
+        if (isalpha(c)) {
+            char base = isupper(c) ? 'A' : 'a';
+            char decryptedChar = static_cast<char>((c - base - shift + 26) % 26 + base);
+            result += decryptedChar;
+        } else {
+            result += c;
+        }
+    }
+
+    return result;
+}
+string encryptVigenereCipher(const string& text, const string& key) {
+    string result = "";
+    int textLength = text.length();
+    int keyLength = key.length();
+
+    for (int i = 0; i < textLength; ++i) {
+        char currentChar = text[i];
+
+        if (isalpha(currentChar)) {
+            char base = isupper(currentChar) ? 'A' : 'a';
+            char encryptedChar = static_cast<char>((currentChar + key[i % keyLength] - 2 * base) % 26 + base);
+            result += encryptedChar;
+        } else {
+            result += currentChar;
+        }
+    }
+
+    return result;
+}
+string decryptVigenereCipher(const string& text, const string& key) {
+    string result = "";
+    int textLength = text.length();
+    int keyLength = key.length();
+
+    for (int i = 0; i < textLength; ++i) {
+        char currentChar = text[i];
+
+        if (isalpha(currentChar)) {
+            char base = isupper(currentChar) ? 'A' : 'a';
+            char decryptedChar = static_cast<char>((currentChar - key[i % keyLength] + 26) % 26 + base);
+            result += decryptedChar;
+        } else {
+            result += currentChar;
         }
     }
 
